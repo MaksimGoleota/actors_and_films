c74e41793cec0cb4198a2c599cb7836ef74f594a
MaksimGoleota <74358982+maksimgoleota@users.noreply.github.com>
2021-07-07 17:27:13 +0300

    AF-1 activity + fragment + navigation

---
+++ app/src/main/java/com/merio/actors_and_films/AFApplication.kt
@@ -0,0 +1,6 @@
+package com.merio.actors_and_films
+
+import android.app.Application
+
+class AFApplication: Application() {
+}
--- app/src/main/java/com/merio/actors_and_films/MainActivity.kt
+++ app/src/main/java/com/merio/actors_and_films/MainActivity.kt
@@ -2,10 +2,20 @@ package com.merio.actors_and_films
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController: NavController by lazy {
+        findNavController(R.id.nav_fragment)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val graph = navController.navInflater.inflate(R.navigation.navigation)
+        navController.graph = graph
     }
 }
